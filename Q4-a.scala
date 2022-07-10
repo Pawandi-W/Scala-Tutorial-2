e2f696dc0969fe7c5e1cc57e9c220e4f684548b9
Pawandi-W <90926516+Pawandi-W@users.noreply.github.com>
2022-07-10 13:33:21 +0530

    Update Q4-a.scala

--- Q4-a.scala
+++ Q4-a.scala
@@ -3,3 +3,7 @@ def ot(hours:Int):Int=hours* 85; // calculate ot amount
 def income(h1:Int,h2:Int):Int= wage(h1) + ot(h2); // calculate income amount
 def tax(income:Int):Double=income* 0.12f; // calculate tax payment fo income
 def takeHome(h1:Int,h2:Int):Double=income(h1,h2)-tax(income(h1,h2)); // calculate final takeHome amount
+
+val input1 = 40;
+val input2 = 30;
+println("Take home amount after working " + input1 + " Normal Hours and " +  input2 + " Hours OT is Rs." + takeHome(input1,input2) );
